4f8dd8ae47f91df65f86e2a8a263a9b769c1b1d8
Barbara Chen <barbara.chen@mail.utoronto.ca>
2021-05-08 15:58:16 -0400

    Level1 Authentication: Username and Password Only

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.listen(3000, function(){
+    console.log("Successfully connect to port 3000");
+});
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.route("/login")
+    .get(function(req,res){
+        res.render("login");
+    })
+    .post(function(req,res){
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({email: username}, function(err,foundUser){
+            if (err) {
+                console.log(err);
+            } else if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        })
+    });
+
+app.route("/register")
+    .get(function(req,res){
+        res.render("register");
+    })
+    .post(function(req,res){
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save(function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    });
