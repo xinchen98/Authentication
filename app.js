2f0b18076e57e9f191c9edbd06b79383bae98906
Barbara Chen <barbara.chen@mail.utoronto.ca>
2021-05-10 20:53:55 -0400

    Level3 Hashing: use md5 for encryption

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(express.static("public"));
@@ -16,8 +16,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -35,7 +33,7 @@ app.route("/login")
     })
     .post(function(req,res){
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
         User.findOne({email: username}, function(err,foundUser){
             if (err) {
                 console.log(err);
@@ -54,7 +52,7 @@ app.route("/register")
     .post(function(req,res){
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
 
         newUser.save(function(err){
