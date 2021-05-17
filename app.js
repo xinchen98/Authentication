17e826b379e8a87d66cf64aba2b87dc48a2ea3bf
Barbara Chen <barbara.chen@mail.utoronto.ca>
2021-05-17 14:15:06 -0400

    Level4 Hashing and Salting: use Salting and saltRounds for encryption

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
@@ -33,14 +34,16 @@ app.route("/login")
     })
     .post(function(req,res){
         const username = req.body.username;
-        const password = md5(req.body.password);
+        const password = req.body.password;
         User.findOne({email: username}, function(err,foundUser){
             if (err) {
                 console.log(err);
             } else if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result == true) {
+                        res.render("secrets");
+                    }
+                });
             }
         })
     });
@@ -50,16 +53,18 @@ app.route("/register")
         res.render("register");
     })
     .post(function(req,res){
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password)
-        });
-
-        newUser.save(function(err){
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
+        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+            const newUser = new User({
+                email: req.body.username,
+                password: hash
+            });
+    
+            newUser.save(function(err){
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render("secrets");
+                }
+            });
         });
     });
